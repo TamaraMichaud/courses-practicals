6765d5f2e1acd29209fafa9ae159bd33ce8fa190
Tamara.Michaud <tamara.michaud@smartfocus.com>
2019-08-02 09:55:31 +0100

    add: git ignore

--- deploy--angularjs-trainings.bat
+++
@@ -1,7 +0,0 @@
-START C:\Users\tamara.michaud\apache-tomcat-8.5.43\bin\shutdown.bat
-if NOT ["%errorlevel%"]==["0"] pause
-
-robocopy "C:\Users\tamara.michaud\Desktop\courses-practicals\angularjs-trainings" "C:\Users\tamara.michaud\apache-tomcat-8.5.43\webapps\angularjs-trainings" /E
-if NOT ["%errorlevel%"]==["0"] pause
-
-C:\Users\tamara.michaud\apache-tomcat-8.5.43\bin\startup.bat
