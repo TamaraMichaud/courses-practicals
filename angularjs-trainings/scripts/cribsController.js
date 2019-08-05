63eee973886e3365429d4238b80440f69f79ed90
TM <tamaramichaud@hotmail.com>
2019-08-05 22:30:56 +0100

    add: ability to add items to my json and display them iteratively

--- angularjs-trainings/scripts/cribsController.js
+++ angularjs-trainings/scripts/cribsController.js
@@ -2,9 +2,25 @@ angular
 	.module('ngCribs')
 	.controller('cribsController', function($scope, cribsFactory) {
 
+	//$scope is an angular built-in for "this" (?)
+	
 	$scope.hello = 'Hello World!';
 	$scope.cribs;
+	
+	$scope.priceInfo = {
+		min: 0,
+		max: 1000000
+	}
 
+	$scope.newListing = {};
+	
+	$scope.addCrib = function(newListing) {
+		newListing.image = "image3";
+		$scope.cribs.data.push(newListing);
+		$scope.newListing = {};
+	}
+	
+	
 	cribsFactory.getCribs().then(function(data) {
 
 		$scope.cribs = data;
@@ -13,10 +29,8 @@ angular
 		console.log(error); 
 	});
 
-	$scope.sayHello = function() {
 
-		console.log("Hello There");
-	}
-
+	
+	
 
 });
